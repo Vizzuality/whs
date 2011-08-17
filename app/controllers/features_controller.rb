950275e5268870f419f376b33e422289c4ad1d5e
Fernando Espinosa <fer@ferdev.com>
2011-08-17 10:16:35 +0200

    Fix a bug in features controller index action

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -5,7 +5,7 @@ class FeaturesController < ApplicationController
 
   def index
     @features = Feature.search(params)
-    @features_json = @features.map{|f| {:lat => f.latitude, :lon => f.latitude, :title => f.title, :id => f.id, :type => f.type} }.to_json.html_safe
+    @features_json = @features.map{|f| {:lat => f.latitude, :lon => f.latitude, :title => f.title, :id => f.cartodb_id, :type => f.type} }.to_json.html_safe
 
     render :partial => 'features' if request.xhr?
   end
