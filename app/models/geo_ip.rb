9a6fa2ee679fcf9d7ab0df8bebc5d8a10d0c27ad
Fernando Espinosa <fer@ferdev.com>
2011-09-12 13:05:56 +0200

    Limit results in ip geolocalization to one row

--- app/models/geo_ip.rb
+++ app/models/geo_ip.rb
@@ -5,6 +5,7 @@ class GeoIp
       FROM geo_ips
       WHERE ip_start <= inetmi('#{ip.sanitize_sql!}','0.0.0.0')
       ORDER BY ip_start DESC
+      LIMIT 1
     SQL
 
     location = results.rows.first
